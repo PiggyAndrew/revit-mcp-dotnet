748d815766065139b228b8510b49a7fa0b925aa7
PiggyYan <87892657+SamllPigYanDong@users.noreply.github.com>
2025-09-16 17:56:21 +0800

    update

--- NET.Mcp.Server/obj/Debug/net8.0/NET.Mcp.Server.AssemblyInfo.cs
+++ NET.Mcp.Server/obj/Debug/net8.0/NET.Mcp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NET.Mcp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebbc7e7098e6525bb9ae3ef537fdc750477256d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b491ac01097f7eeb6c9736754793f12e33390cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NET.Mcp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NET.Mcp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
